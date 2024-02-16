85ef341e755860c964047a1d590a29f6b4914f27
csoeylemez <CSoeylemez@bhakwien10.at>
2024-02-16 10:16:10 +0100

    Projektdateien hinzufügen.

---
+++ netmaui2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace netmaui2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
